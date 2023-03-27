d2e57a986b93a7f73e274499aab52eb65fc5c861
Sofja Hohlova <s21hohlsofj@vea.lv>
2023-03-27 11:36:18 +0300

    Add: isFull, isEmpty, howManyElements, increaseArray

--- src/module/MyGraph.java
+++ src/module/MyGraph.java
@@ -1,5 +1,36 @@
 package module;
 
-public class MyGraph {
-
+public class MyGraph<T> {
+	private MyVerticeNode[] graphElements;
+	private final int DEFAULT_ARRAY_SIZE = 10;
+	private int arraySize = DEFAULT_ARRAY_SIZE;
+	private int elementCounter = 0;
+	
+	//no-args
+	public MyGraph() {
+		graphElements = new MyVerticeNode[arraySize];
+	}
+	
+	//args
+	public MyGraph(int inputArraySize) {
+		if(inputArraySize > 0) {
+			arraySize = inputArraySize;
+		}
+		graphElements = new MyVerticeNode[arraySize];
+	}
+	
+	//TODO isFull, isEmpty, howManyElements, increaseArray
+	
+	public boolean isFull() {
+		return (elementCounter == arraySize);
+	}
+	
+	public boolean isEmpty() {
+		return (elementCounter == 0);
+	}
+	
+	public int howManyElements() {
+		return elementCounter;
+	}
+	
 }
--- src/module/MyVerticeNode.java
+++ src/module/MyVerticeNode.java
@@ -1,5 +1,39 @@
 package module;
 
-public class MyVerticeNode {
-
+public class MyVerticeNode <T>{
+	private T element;
+	private MyEdgeNode firstEdge = null;
+	
+	
+	public T getElement() {
+		return element;
+	}
+	
+	public void setElement(T inputElement) {
+		if(inputElement != null) {
+			element = inputElement;
+		}
+		else {
+			element = (T) new Object();
+		}
+	}
+	
+	public MyEdgeNode getFirstEdge() {
+		return firstEdge;
+	}
+	
+	public void setFirstEdge(MyEdgeNode firstEdge) {
+		this.firstEdge = firstEdge;
+	}
+	
+	//TODO constructors and toString
+	
+	public MyVerticeNode(T element) {
+		setElement(element);
+	}
+	
+	public String toString() {
+		return "" + element;
+	}
+	
 }
